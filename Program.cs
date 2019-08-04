2432d81f0a4f504e4171952761c83ab01099609a
WhiteAlchemist <enushi5@gmail.com>
2019-08-04 23:48:36 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+static void Main(string[] args)
+{
+    int myInt = 5;
+    int yourInt = 10;
+
+    int total = myInt + yourInt;
+
+    Console.WriteLine(total);
+
+    Console.WriteLine("Hello World!");
+    Console.WriteLine("Hello World!");
+    Console.WriteLine("Hello World!");
+}
+}
